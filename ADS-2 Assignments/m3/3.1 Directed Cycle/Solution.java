f199eeb36820d555fa7de30bc7c6a9270a2cfeaa
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-11-01 00:34:02 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/m3/3.1 Directed Cycle/Solution.java
+++ ADS-2 Assignments/m3/3.1 Directed Cycle/Solution.java
@@ -1,34 +1,32 @@
 import java.util.Scanner;
-/**.
- * Solution class.
- **/
- final class Solution {
- 	/**.
- 	 * Default constructor.
- 	 **/
- 	private Solution() {
- 		//unsed.
- 	}
- 	/**
- 	 * @param args String.
- 	 */
- 	public static void main(final String[] args) {
- 		Scanner sc =new Scanner(System.in);
 
- 		int vertex = Integer.parseInt(sc.nextLine());
+public final class Solution {
+
+    private Solution() {
+        //unused
+    }
+    /**.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vertex = Integer.parseInt(sc.nextLine());
         int edge = Integer.parseInt(sc.nextLine());
         Digraph digraph = new Digraph(vertex);
- 		DirectedCycle directedcycle = new DirectedCycle(digraph);
- 		for (int i = 0; i < edge; i++) {
- 			String[] input = sc.nextLine().split(" ");
-            digraph.addEdge(Integer.parseInt(input[0]),
-                         Integer.parseInt(input[1]));
- 		}
- 		if(directedcycle.hasCycle()) {
- 			System.out.println("Cycle exists.");
- 		} else {
- 			System.out.println("Cycle doesn't exists.");
- 		}
+        for(int i=0;i<edge;i++) {
+            String[] tokens = sc.nextLine().split(" ");
+            digraph.addEdge(Integer.parseInt(tokens[0]),
+                         Integer.parseInt(tokens[1]));
+        }
+        DirectedCycle directed = new DirectedCycle(digraph);
+        if (directed.hasCycle()) {
+            System.out.println("Cycle exists.");
+        }
+        else {
+            System.out.println("Cycle doesn't exists.");
 
- 	}
- }
+        }
+
+    }
+}

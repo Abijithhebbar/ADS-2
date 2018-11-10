55bf4794450018a4d39acc1989c6971311013f76
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-11-10 12:21:20 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2 Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,6 +1,19 @@
-import java.util.*;
-public class Solution {
+import java.util.Scanner;
+import java.util.ArrayList;
+/**
+ Solution class.
+ */
+final class Solution {
+	/**
+	 default constructor.
+	 */
+private Solution() {
 
+}
+/**
+ * Main method.
+ * @param args String.
+ */
 	public static void main(String[] args) {
 		// Self loops are not allowed...
 		// Parallel Edges are allowed...
@@ -11,7 +24,8 @@ public class Solution {
 		EdgeWeightedGraph weightedgraph = new EdgeWeightedGraph(vertex);
 		for (int i = 0; i < edges; i++) {
 			String[] inp = sc.nextLine().split(" ");
-			weightedgraph.addEdge(new Edge(Integer.parseInt(inp[0]), Integer.parseInt(inp[1]), Double.valueOf(inp[2])));
+			weightedgraph.addEdge(new Edge(Integer.parseInt(inp[0]),
+				Integer.parseInt(inp[1]), Double.valueOf(inp[2])));
 		}
 
 		String caseToGo = sc.nextLine();
@@ -26,9 +40,10 @@ public class Solution {
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
 			String[] inp = sc.nextLine().split(" ");
-			DijkstraUndirectedSP shortpath = new DijkstraUndirectedSP(weightedgraph, Integer.parseInt(inp[0]));
+			DijkstraUndirectedSP shortpath = new DijkstraUndirectedSP(
+				weightedgraph, Integer.parseInt(inp[0]));
 			if (shortpath.hasPathTo(Integer.parseInt(inp[1]))) {
-        		System.out.println(shortpath.distTo(Integer.parseInt(inp[1])));
+        	System.out.println(shortpath.distTo(Integer.parseInt(inp[1])));
      			 } else {
         		System.out.println("No Path Found.");
       			}
@@ -36,7 +51,8 @@ public class Solution {
 
 		case "ViaPaths":
 			// Handle the case of middlePaths, where three integers are given.
-			// First is the source and second is the middle is the one where path should pass throuh.
+			// First is the source and second is the middle is the one
+			// where path should pass throuh.
 			// third is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
@@ -44,20 +60,21 @@ public class Solution {
             int start = Integer.parseInt(input[0]);
             int middle = Integer.parseInt(input[1]);
             int end = Integer.parseInt(input[2]);
-            DijkstraUndirectedSP viapath = new DijkstraUndirectedSP(weightedgraph, start);
+            DijkstraUndirectedSP viapath = new DijkstraUndirectedSP(
+            	weightedgraph, start);
             double totalSum = 0.0;
-            ArrayList<Integer> path = new ArrayList<>();
+            ArrayList<Integer> route = new ArrayList<>();
             if (viapath.hasPathTo(end)) {
                 if (viapath.hasPathTo(middle)) {
-                    path.add(start);
+                    route.add(start);
                     totalSum += viapath.distTo(middle);
                     for (Edge e : viapath.pathTo(middle)) {
                         int temp = e.either();
-                        if (!path.contains(temp)) {
-                            path.add(temp);
+                        if (!route.contains(temp)) {
+                            route.add(temp);
                         }
-                        if (!path.contains(e.other(temp))) {
-                            path.add(e.other(temp));
+                        if (!route.contains(e.other(temp))) {
+                            route.add(e.other(temp));
                         }
                     }
                     viapath = new DijkstraUndirectedSP(weightedgraph, middle);
@@ -65,17 +82,17 @@ public class Solution {
                         totalSum += viapath.distTo(end);
                         for (Edge e : viapath.pathTo(end)) {
                             int temp = e.either();
-                            if (!path.contains(temp)) {
-                                path.add(temp);
+                            if (!route.contains(temp)) {
+                                route.add(temp);
                             }
-                            if (!path.contains(e.other(temp))) {
-                                path.add(e.other(temp));
+                            if (!route.contains(e.other(temp))) {
+                                route.add(e.other(temp));
                             }
                         }
                     }
                 }
                 System.out.println(totalSum);
-                String out = path.toString().replaceAll(",", "");
+                String out = route.toString().replaceAll(",", "");
                 out = out.substring(1, out.length() - 1);
                 System.out.println(out);
             } else {

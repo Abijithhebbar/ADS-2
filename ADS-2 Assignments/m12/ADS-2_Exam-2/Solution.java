8cf768d0bfd8a5d33c52a02fd363fc2c8fe0b281
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-11-10 12:06:30 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2 Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -28,19 +28,60 @@ public class Solution {
 			String[] inp = sc.nextLine().split(" ");
 			DijkstraUndirectedSP shortpath = new DijkstraUndirectedSP(weightedgraph, Integer.parseInt(inp[0]));
 			if (shortpath.hasPathTo(Integer.parseInt(inp[1]))) {
-        System.out.println(shortpath.distTo(Integer.parseInt(inp[1])));
-      } else {
-        System.out.println("No Path Found.");
-      }
+        		System.out.println(shortpath.distTo(Integer.parseInt(inp[1])));
+     			 } else {
+        		System.out.println("No Path Found.");
+      			}
 			break;
 
 		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
+			// Handle the case of middlePaths, where three integers are given.
+			// First is the source and second is the middle is the one where path should pass throuh.
 			// third is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
-			break;
+		 String[] input = sc.nextLine().split(" ");
+            int start = Integer.parseInt(input[0]);
+            int middle = Integer.parseInt(input[1]);
+            int end = Integer.parseInt(input[2]);
+            DijkstraUndirectedSP viapath = new DijkstraUndirectedSP(weightedgraph, start);
+            double totalSum = 0.0;
+            ArrayList<Integer> path = new ArrayList<>();
+            if (viapath.hasPathTo(end)) {
+                if (viapath.hasPathTo(middle)) {
+                    path.add(start);
+                    totalSum += viapath.distTo(middle);
+                    for (Edge e : viapath.pathTo(middle)) {
+                        int temp = e.either();
+                        if (!path.contains(temp)) {
+                            path.add(temp);
+                        }
+                        if (!path.contains(e.other(temp))) {
+                            path.add(e.other(temp));
+                        }
+                    }
+                    viapath = new DijkstraUndirectedSP(weightedgraph, middle);
+                    if (viapath.hasPathTo(end)) {
+                        totalSum += viapath.distTo(end);
+                        for (Edge e : viapath.pathTo(end)) {
+                            int temp = e.either();
+                            if (!path.contains(temp)) {
+                                path.add(temp);
+                            }
+                            if (!path.contains(e.other(temp))) {
+                                path.add(e.other(temp));
+                            }
+                        }
+                    }
+                }
+                System.out.println(totalSum);
+                String out = path.toString().replaceAll(",", "");
+                out = out.substring(1, out.length() - 1);
+                System.out.println(out);
+            } else {
+                System.out.println("No Path Found.");
+            }
+            break;
 
 		default:
 			break;

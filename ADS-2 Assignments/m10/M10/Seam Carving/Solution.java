cb8670c0da8eaff63104023ca767c92c659a1c95
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-11-11 22:15:18 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/m10/M10/Seam Carving/Solution.java
+++ ADS-2 Assignments/m10/M10/Seam Carving/Solution.java
@@ -24,8 +24,9 @@ final class Solution {
         System.out.printf("Printing energy calculated for each pixel.\n");
 
         for (int row = 0; row < sc.height(); row++) {
-            for (int col = 0; col < sc.width(); col++)
+            for (int col = 0; col < sc.width(); col++) {
                 System.out.printf("%9.0f ", sc.energy(col, row));
+            }
             System.out.println();
         }
     }
@@ -43,8 +44,10 @@ final class Solution {
             for (int col = 0; col < carver.width(); col++) {
                 double energy = carver.energy(col, row);
                 String marker = " ";
-                if ((direction == true && row == seam[col]) ||
-                        (direction == false   && col == seam[row])) {
+                if ((direction == true && row == seam[col])) {
+                    marker = "*";
+                    totalSeamEnergy += energy;
+                } else if ((direction == false   && col == seam[row])) {
                     marker = "*";
                     totalSeamEnergy += energy;
                 }
@@ -69,8 +72,8 @@ final class Solution {
             case "width":
                 while (scan.hasNextLine()) {
                     String file = scan.nextLine();
-                    seamCarver = new SeamCarver(new Picture
-                        ("/Files/" + file));
+                    seamCarver = new SeamCarver(new Picture(
+                        "/Files/" + file));
                     System.out.println(seamCarver.width());
                 }
                 break;
@@ -78,8 +81,8 @@ final class Solution {
             case "height":
                 while (scan.hasNextLine()) {
                     String file = scan.nextLine();
-                    seamCarver = new SeamCarver
-                    (new Picture("/Files/" + file));
+                    seamCarver = new SeamCarver(
+                        new Picture("/Files/" + file));
                     System.out.println(seamCarver.height());
                 }
                 break;

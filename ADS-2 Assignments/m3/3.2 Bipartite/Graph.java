9b8cb71d7dbde4c49ec47d2c3828c6b37a9ee074
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-11-27 17:11:42 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 Assignments/m3/3.2 Bipartite/Graph.java
+++ ADS-2 Assignments/m3/3.2 Bipartite/Graph.java
@@ -1,5 +1,5 @@
 /*************************************************************************
- *  Compilation:  javac Graph.java        
+ *  Compilation:  javac Graph.java
  *  Execution:    java Graph input.txt
  *  Dependencies: Bag.java In.java StdOut.java
  *  Data files:   http://algs4.cs.princeton.edu/41undirected/tinyG.txt
@@ -8,28 +8,28 @@
  *  Parallel edges and self-loops allowed.
  *
  *  % java Graph tinyG.txt
- *  13 vertices, 13 edges 
- *  0: 6 2 1 5 
- *  1: 0 
- *  2: 0 
- *  3: 5 4 
- *  4: 5 6 3 
- *  5: 3 4 0 
- *  6: 0 4 
- *  7: 8 
- *  8: 7 
- *  9: 11 10 12 
- *  10: 9 
- *  11: 9 12 
- *  12: 11 9 
+ *  13 vertices, 13 edges
+ *  0: 6 2 1 5
+ *  1: 0
+ *  2: 0
+ *  3: 5 4
+ *  4: 5 6 3
+ *  5: 3 4 0
+ *  6: 0 4
+ *  7: 8
+ *  8: 7
+ *  9: 11 10 12
+ *  10: 9
+ *  11: 9 12
+ *  12: 11 9
  *
  *  % java Graph mediumG.txt
- *  250 vertices, 1273 edges 
- *  0: 225 222 211 209 204 202 191 176 163 160 149 114 97 80 68 59 58 49 44 24 15 
- *  1: 220 203 200 194 189 164 150 130 107 72 
- *  2: 141 110 108 86 79 51 42 18 14 
+ *  250 vertices, 1273 edges
+ *  0: 225 222 211 209 204 202 191 176 163 160 149 114 97 80 68 59 58 49 44 24 15
+ *  1: 220 203 200 194 189 164 150 130 107 72
+ *  2: 141 110 108 86 79 51 42 18 14
  *  ...
- *  
+ *
  *************************************************************************/
 
 
@@ -47,7 +47,7 @@ public class Graph {
     private final int V;
     private int E;
     private Bag<Integer>[] adj;
-    
+
    /**
      * Create an empty graph with V vertices.
      */
@@ -75,7 +75,7 @@ public class Graph {
         }
     }
 
-   /**  
+   /**
      * Create a digraph from input stream.
      */
 /*    public Graph(In in) {
@@ -109,7 +109,7 @@ public class Graph {
    /**
      * Return the number of vertices in the graph.
      */
-    public int V() { return V; }
+    public int vert() { return V; }
 
    /**
      * Return the number of edges in the graph.
